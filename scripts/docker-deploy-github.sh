c4e6f67d58ea64f3b3f0537ce99a6e822633a417
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2020-08-01 19:08:30 +0100

    add more ci scripts

--- scripts/docker-deploy-github.sh
+++ scripts/docker-deploy-github.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "${{ secrets.GH_TOKEN }}" | docker login -u lfmachadodasilva --password-stdin docker.pkg.github.com
+echo "$GH_TOKEN" | docker login -u lfmachadodasilva --password-stdin docker.pkg.github.com
 docker tag $GITHUB_REPOSITORY:$GITHUB_RUN_NUMBER docker.pkg.github.com/$GITHUB_REPOSITORY/${GITHUB_REF##*/}:$GITHUB_RUN_NUMBER
 docker tag $GITHUB_REPOSITORY:$GITHUB_RUN_NUMBER docker.pkg.github.com/$GITHUB_REPOSITORY/${GITHUB_REF##*/}:latest
 docker push docker.pkg.github.com/$GITHUB_REPOSITORY/${GITHUB_REF##*/}:$GITHUB_RUN_NUMBER
