e8622801b815788d5995793b99fbe554a83ae0e0
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2020-08-03 08:03:34 +0100

    docker changes

--- scripts/docker-heroku-prod.sh
+++ scripts/docker-heroku-prod.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker tag $GITHUB_REPOSITORY:$GITHUB_RUN_NUMBER registry.heroku.com/$HEROKU_APP_NAME/web
-docker push registry.heroku.com/$HEROKU_APP_NAME/web
-#heroku container:push web --app $HEROKU_APP_NAME
+#docker push registry.heroku.com/$HEROKU_APP_NAME/web
+heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
