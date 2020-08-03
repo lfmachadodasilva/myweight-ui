8f88329e6c0d4c7dca1ad2df2d1b25c36539766d
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2020-08-03 06:06:43 +0100

    update login heroku

--- scripts/docker-heroku.sh
+++ scripts/docker-heroku.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 wget -qO- https://cli-assets.heroku.com/install.sh | sh
-echo "$HEROKU_API_KEY" | docker login -u "_" --password-stdin registry.heroku.com
+docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
