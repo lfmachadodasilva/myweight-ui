931ce8f91f47ff18df00230e059f7d4c827a747f
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2020-08-03 07:14:07 +0100

    docker changes

--- scripts/docker-heroku-dev.sh
+++ scripts/docker-heroku-dev.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-wget -qO- https://cli-assets.heroku.com/install.sh | sh
-echo "$HEROKU_PASSWORD" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
-
 docker tag $GITHUB_REPOSITORY:$GITHUB_RUN_NUMBER registry.heroku.com/$HEROKU_APP_NAME-dev/web
-docker push registry.heroku.com/$HEROKU_APP_NAME-dev/web
+# docker push registry.heroku.com/$HEROKU_APP_NAME-dev/web
+heroku container:push web
 heroku container:release web --app $HEROKU_APP_NAME-dev
--- scripts/docker-heroku-prod.sh
+++ scripts/docker-heroku-prod.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# already done by docker-heroku-dev.sh
-# wget -qO- https://cli-assets.heroku.com/install.sh | sh
-# echo "$HEROKU_PASSWORD" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
-
 docker tag $GITHUB_REPOSITORY:$GITHUB_RUN_NUMBER registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web
 heroku container:release web --app $HEROKU_APP_NAME
